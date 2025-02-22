c4fd53c17f96c90694dde488e08304c93936e87e
Anthony <antnay@uw.edu>
2025-02-22 11:14:39 -0800

    update ignore

---
+++ server/jobpost.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+
+def postJob():
+    browser = webdriver.Firefox()
+    browser.get('https://github.com/ytang07/ai_agents_cookbooks/blob/main/llamaindex/openai_rag_agent_w_evals.ipynb')
+
+    time.sleep(2);
+    browser.close()
+
+postJob()
