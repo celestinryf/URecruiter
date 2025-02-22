e4efdf2499f16989cbee9dd6be6bf5072e182a87
celestinryf <celestinryf@gmail.com>
2025-02-22 11:29:15 -0800

    feat: static job description generation (not connected to LLM)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
-import Jobpost from './routes/Jobpost';
-import Appreview from './routes/Appreview';
+// import Jobpost from './routes/Jobpost';
+// import Appreview from './routes/Appreview';
 
 function App() {
   return (
     <Routes>
-      {/* <PageLayout> */}
         <Route path="/" element={<Home />} />
-        <Route path="/jobpost" element={<Jobpost />} />
-        <Route path="/appreview" element={<Appreview />} />
-      {/* </PageLayout> */}
+        {/* <Route path="/jobpost" element={<Jobpost />} />
+        <Route path="/appreview" element={<Appreview />} /> */}
     </Routes>
   );
 }
