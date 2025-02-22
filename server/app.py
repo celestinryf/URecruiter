2a77e6ccfe2ddd2a280fed368f1e2f50bda11966
Anthony <antnay@uw.edu>
2025-02-22 11:35:35 -0800

    add routes

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-
+from jobpost import postJob
+from applicant_recruiting import JobDescriptionGenerator
 
 load_dotenv()
 
@@ -9,6 +10,14 @@ load_dotenv()
 app = Flask(__name__)
 
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:5173"}}, supports_credentials=True)
+
+@app.get("/generate-description", methods=["POST"])
+def generateDesc():
+    JobDescriptionGenerator.generate(title=, company=, keywords=, experience=)
+
+@app.route("/generate-post", methods=["POST"])
+def generatePost():
+    postJob()
 
 if __name__ == "__main__":
     app.run(debug=True)
